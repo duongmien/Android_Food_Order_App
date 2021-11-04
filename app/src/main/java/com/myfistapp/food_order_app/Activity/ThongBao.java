e42b32330f6ce3f7c066403b6faacaff066a322f
nguyenson257 <nguyenson25701@gmail.com>
2021-11-04 23:41:54 +0700

    them dia chi

--- app/src/main/java/com/myfistapp/food_order_app/Activity/ThongBao.java
+++ app/src/main/java/com/myfistapp/food_order_app/Activity/ThongBao.java
@@ -1,28 +1,25 @@
 package com.myfistapp.food_order_app.Activity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.navigation.NavigationBarView;
 import com.myfistapp.food_order_app.R;
 
-public class ThongTinCaNhan extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
+public class ThongBao extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thong_tin_ca_nhan);
+        setContentView(R.layout.activity_thong_bao);
         bottomNavigation();
-        }
+    }
+
+
 
     private void bottomNavigation() {
         FloatingActionButton floatingActionButton = findViewById(R.id.card_btn);
@@ -34,39 +31,37 @@ public class ThongTinCaNhan extends AppCompatActivity {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ThongTinCaNhan.this, GioHang.class));
+                startActivity(new Intent(ThongBao.this, GioHang.class));
             }
         });
 
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ThongTinCaNhan.this, TrangChu.class));
+                startActivity(new Intent(ThongBao.this, TrangChu.class));
             }
         });
 
         favBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ThongTinCaNhan.this, YeuThich.class));
+                startActivity(new Intent(ThongBao.this, YeuThich.class));
             }
         });
 
         notiBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ThongTinCaNhan.this, ThongBao.class));
+                startActivity(new Intent(ThongBao.this, ThongBao.class));
             }
         });
 
         proBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ThongTinCaNhan.this, ThongTinCaNhan.class));
+                startActivity(new Intent(ThongBao.this, ThongTinCaNhan.class));
             }
         });
-
-    }
 
     }
 }
